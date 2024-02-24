299c9c57dd7fb30d3746b8fea25d11d934a94a49
Chidera Ibe <chideraibe482005@gmail.com>
2024-02-24 15:46:30 -0600

    fixed it

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('chatgdp.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ChatGDP!');
+		vscode.window.showInformationMessage('Hello from ChatGDP!');
 	});
 
 	context.subscriptions.push(disposable);
